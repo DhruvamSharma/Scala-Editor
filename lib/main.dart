abe3c654043ecb239c5104fea4a705ee496d5d74
DhruvamSharma <dhruvamssharma@gmail.com>
2023-03-17 16:43:58 +0530

    added final changes for editor

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:scala_editor/filter_selector.dart';
 
 void main() {
@@ -19,27 +18,15 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  void initState() {
-    SystemChrome.setSystemUIOverlayStyle(
-        const SystemUiOverlayStyle(statusBarColor: Colors.black));
-    super.initState();
-  }
+class MyHomePage extends StatelessWidget {
+  MyHomePage({
+    super.key,
+  });
 
   final _filters = [
     Colors.white,
@@ -55,19 +42,18 @@ class _MyHomePageState extends State<MyHomePage> {
     _filterColor.value = value;
   }
 
-  static const String _imageLink = 'https://docs.flutter.dev/cookbook/img-files/effects/instagram-buttons/millenial-dude.jpg';
+  static const String _imageLink =
+      'https://docs.flutter.dev/cookbook/img-files/effects/instagram-buttons/millenial-dude.jpg';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Stack(
-        clipBehavior: Clip.none,
         children: [
-
           Center(
             child: Container(
-              height: MediaQuery.of(context).size.height/2,
+              height: MediaQuery.of(context).size.height / 2,
               decoration: const BoxDecoration(
                 image: DecorationImage(
                   image: NetworkImage(
@@ -79,7 +65,8 @@ class _MyHomePageState extends State<MyHomePage> {
               child: BackdropFilter(
                 filter: ImageFilter.blur(sigmaX: 40.0, sigmaY: 40.0),
                 child: Container(
-                  decoration: BoxDecoration(color: Colors.white.withOpacity(0.0)),
+                  decoration:
+                      BoxDecoration(color: _filterColor.value.withOpacity(0.0)),
                 ),
               ),
             ),
@@ -87,20 +74,21 @@ class _MyHomePageState extends State<MyHomePage> {
           Positioned(
             left: 0,
             right: 0,
-            top: 56,
-            child: SafeArea(child: _buildHeader()),
+            top: kToolbarHeight,
+            child: SafeArea(child: _buildHeader(context)),
           ),
           Center(
             child: Container(
               margin: const EdgeInsets.all(60),
               clipBehavior: Clip.antiAlias,
               decoration: BoxDecoration(
-                  shape: BoxShape.rectangle,
-                  borderRadius: BorderRadius.circular(16),
-                  border: Border.all(
-                    color: _filterColor.value,
-                    width: 2.0,
-                  )),
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(16),
+                border: Border.all(
+                  color: _filterColor.value,
+                  width: 10.0,
+                ),
+              ),
               child: _buildPhotoWithFilter(),
             ),
           ),
@@ -134,13 +122,14 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Widget _buildHeader() {
+  Widget _buildHeader(BuildContext context) {
     return Center(
       child: Text(
-        'Chroma Editor',
-        style: Theme.of(context).textTheme.headlineMedium?.copyWith(
-              color: Colors.white,
-            ),
+        'Chroma',
+        style: Theme.of(context)
+            .textTheme
+            .headlineMedium
+            ?.copyWith(color: Colors.white),
       ),
     );
   }
